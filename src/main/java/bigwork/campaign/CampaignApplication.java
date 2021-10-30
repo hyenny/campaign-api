84b1998c344d0a208ea906c1caa5a6456226bb0c
hyenny <qlwb9191@naver.com>
2021-10-30 20:01:30 +0900

    Initial commit

---
+++ src/main/java/bigwork/campaign/CampaignApplication.java
@@ -0,0 +1,13 @@
+package bigwork.campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bigwork/campaign/CampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package bigwork.campaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
