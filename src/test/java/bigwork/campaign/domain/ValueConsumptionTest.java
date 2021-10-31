33d74a72fa8e2e7644078bb58a9c01bd9e6e197e
hyenny <qlwb9191@naver.com>
2021-10-31 17:27:35 +0900

    test: Assertion static import, campaignService 생성시 필수

--- src/test/java/bigwork/campaign/domain/ValueConsumptionTest.java
+++ src/test/java/bigwork/campaign/domain/ValueConsumptionTest.java
@@ -1,10 +1,12 @@
 package bigwork.campaign.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ValueConsumptionTest {
 
@@ -14,46 +16,49 @@ class ValueConsumptionTest {
     @DisplayName("[필수값] 제목 누락")
     @Test
     void 제목_NULL_체크() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             ValueConsumption valueConsumption = ValueConsumption.builder()
                     .startDate(START_DATE)
                     .endDate(END_DATE)
                     .notice("ONLY 빅워크 유저를 위한 가치소비 프로모션")
                     .imagePath("/images/test.jpg")
+                    .campaignService(new CampaignService(CampaignType.VALUE_CONSUMPTION))
                     .build();
         });
 
-        Assertions.assertEquals("제목이 없습니다.", exception.getMessage());
+        assertEquals("제목이 없습니다.", exception.getMessage());
     }
 
     @DisplayName("[필수값] 상세이미지 누락")
     @Test
     void 상세이미지_NULL_체크() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             ValueConsumption valueConsumption = ValueConsumption.builder()
                     .startDate(START_DATE)
                     .endDate(END_DATE)
                     .title("반려견을 위한 진짜 음식")
                     .notice("ONLY 빅워크 유저를 위한 가치소비 프로모션")
+                    .campaignService(new CampaignService(CampaignType.VALUE_CONSUMPTION))
                     .build();
         });
 
-        Assertions.assertEquals("상세 이미지가 없습니다.", exception.getMessage());
+        assertEquals("상세 이미지가 없습니다.", exception.getMessage());
     }
 
     @DisplayName("[필수값] 유의사항 누락")
     @Test
     void 유의사항_NULL_체크() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             ValueConsumption valueConsumption = ValueConsumption.builder()
                     .startDate(START_DATE)
                     .endDate(END_DATE)
                     .title("반려견을 위한 진짜 음식")
                     .imagePath("/images/test.jpg")
+                    .campaignService(new CampaignService(CampaignType.VALUE_CONSUMPTION))
                     .build();
         });
 
-        Assertions.assertEquals("유의사항이 없습니다.", exception.getMessage());
+        assertEquals("유의사항이 없습니다.", exception.getMessage());
     }
 
 }
