968490bcd6f5bb5a36d053f7b4a5c2cbb94f368c
hyenny <qlwb9191@naver.com>
2021-11-01 13:49:03 +0900

    feat: @ModelAttribute 에러 핸들러 추가

---
+++ src/main/java/bigwork/campaign/error/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package bigwork.campaign.error;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 바인딩 에러 (@ModelAttribute)
+     */
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.error("handleBindException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+}
