a305e5d1030203bda24c14189adba989d5d7200d
hyenny <qlwb9191@naver.com>
2021-11-01 22:38:44 +0900

    feat: 초기 데이터 ApplicationRunner로 init 변경, 첨부파일 사이즈 제한

--- src/main/java/bigwork/campaign/CampaignApplication.java
+++ src/main/java/bigwork/campaign/CampaignApplication.java
@@ -1,13 +1,26 @@
 package bigwork.campaign;
 
+import bigwork.campaign.domain.CampaignService;
+import bigwork.campaign.domain.CampaignType;
+import bigwork.campaign.repository.CampaignServiceRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CampaignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CampaignApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(CampaignServiceRepository campaignServiceRepository) {
+        return args -> {
+            // VALUE_CONSUMPTION 타입의 캠페인 서비스 데이터를 넣어준다.
+            campaignServiceRepository.save(new CampaignService(CampaignType.VALUE_CONSUMPTION));
+        };
     }
 
 }
