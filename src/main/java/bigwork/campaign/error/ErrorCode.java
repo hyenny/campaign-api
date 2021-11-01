7cb0a67e5a69056532379885fe09b992b3d47894
hyenny <qlwb9191@naver.com>
2021-11-01 13:48:12 +0900

    feat: 에러코드, 에러응답 추가

---
+++ src/main/java/bigwork/campaign/error/ErrorCode.java
@@ -0,0 +1,32 @@
+package bigwork.campaign.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", "Invalid Input Value");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+}
